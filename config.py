e80b2163f74c98eee033310ed46504025926fd40
N Gautam Animesh <nanimesh@tataunistore.com>
2022-04-30 20:32:51 +0530

    Updated Location

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
 filename = ""
-finalfile ="/Users/SHIVANSH/Desktop/CapstoneProject/CapstoneProject/Digital Invoicing and Tax Filling System/download/ITR-4.pdf"
+finalfile ="C:/Users/Gautam/OneDrive/Desktop/Digital Invoicing and Tax Filling System/download/ITR-4.pdf"
